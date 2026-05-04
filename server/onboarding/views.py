e24a8ceea9b972e62ccd064328e7ac4da6f5b1de
maciejlug <maciej.lugowski5@gmail.com>
2026-05-04 14:11:48 +0200

    fix: use relative import for onboarding email helper

--- server/onboarding/views.py
+++ server/onboarding/views.py
@@ -14,7 +14,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
-from server.onboarding.emails import send_verification_email
+from .emails import send_verification_email
 
 from .models import OnboardingSession
 from .serializers import (
